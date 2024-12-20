ca37420aa448c3f5e4d8746193694c4e73df3505
Taisuke Miyazaki <imishinist@gmail.com>
2024-12-20 10:08:35 +0900

    add writer

--- cmd/listObjects.go
+++ cmd/listObjects.go
@@ -1,28 +1,119 @@
 package cmd
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
 	"log/slog"
 	"sync"
+	"time"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/s3/types"
+	"github.com/google/uuid"
 	"github.com/spf13/cobra"
+	"github.com/thediveo/enumflag/v2"
 
 	"saw/internal"
 )
+
+type ListObjectsWriter interface {
+	WriteObject(out io.Writer, bucket string, object types.Object) error
+}
+
+type ListObjectsPlainWriter struct{}
+
+func (w *ListObjectsPlainWriter) WriteObject(out io.Writer, bucket string, object types.Object) error {
+	_, err := fmt.Fprintf(out, "%s/%s\n", bucket, *object.Key)
+	return err
+}
+
+type ListObjectsCwlS3EventWriter struct {
+	DetailType string
+	Reason     string
+}
+
+func (w *ListObjectsCwlS3EventWriter) WriteObject(out io.Writer, bucket string, object types.Object) error {
+	detail := internal.S3EventDetail{
+		Version: "0",
+		Bucket: internal.S3Bucket{
+			Name: bucket,
+		},
+		Object: internal.S3Object{
+			Key:  *object.Key,
+			Size: *object.Size,
+			ETag: *object.ETag,
+		},
+		RequestID: "",
+		Requester: "",
+		SourceIP:  "",
+		Reason:    w.Reason,
+	}
+	b, err := json.Marshal(detail)
+	if err != nil {
+		return err
+	}
+
+	evt := events.CloudWatchEvent{
+		Version:    "0",
+		ID:         uuid.New().String(),
+		DetailType: w.DetailType,
+		Source:     "aws.s3",
+		Time:       time.Now(),
+		AccountID:  "",
+		Region:     "",
+		Resources: []string{
+			fmt.Sprintf("arn:aws:s3:::%s", bucket),
+		},
+		Detail: b,
+	}
+	t, err := json.Marshal(evt)
+	if err != nil {
+		return err
+	}
+	_, err = fmt.Fprintf(out, "%s\n", string(t))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+//go:generate go run github.com/dmarkham/enumer -type=ListObjectsFormat -json -text -yaml -trimprefix=ListObjectsFormat -transform=kebab
+type ListObjectsFormat enumflag.Flag
+
+const (
+	ListObjectsFormatPlain ListObjectsFormat = iota
+	ListObjectsFormatCwlS3Event
+)
+
+var ListObjectsFormatStringMap = map[ListObjectsFormat][]string{
+	ListObjectsFormatPlain:      {"plain"},
+	ListObjectsFormatCwlS3Event: {"cwl-s3-event"},
+}
 
 type ListObjectsOpts struct {
 	Bucket string
 
 	Prefixes  []string
 	Delimiter string
+
+	Format ListObjectsFormat
+
+	// for cwl-s3-event
+	DetailType string
+	Reason     string
 }
 
 var (
 	listObjectsOpts = &ListObjectsOpts{
-		Bucket:    "",
-		Prefixes:  []string{""},
-		Delimiter: "",
+		Bucket:     "",
+		Prefixes:   []string{""},
+		Delimiter:  "",
+		Format:     ListObjectsFormatPlain,
+		DetailType: "Object Created",
+		Reason:     "PutObject",
 	}
 )
 
@@ -39,12 +130,18 @@ var listObjectsCmd = &cobra.Command{
 		if err != nil {
 			return err
 		}
-
 		client := s3.NewFromConfig(cfg)
+		wg := new(sync.WaitGroup)
+
+		writer := ListObjectsWriter(&ListObjectsPlainWriter{})
+		if listObjectsOpts.Format == ListObjectsFormatCwlS3Event {
+			writer = ListObjectsWriter(&ListObjectsCwlS3EventWriter{
+				DetailType: listObjectsOpts.DetailType,
+				Reason:     listObjectsOpts.Reason,
+			})
+		}
 
 		prefixes := internal.SliceCh(listObjectsOpts.Prefixes)
-
-		wg := new(sync.WaitGroup)
 		for i := 0; i < concurrency; i++ {
 			wg.Add(1)
 			go func() {
@@ -64,12 +161,11 @@ var listObjectsCmd = &cobra.Command{
 							return
 						}
 
-						// TODO: print s3 event jsonl
-						for _, p := range output.CommonPrefixes {
-							cmd.Println(*p.Prefix)
-						}
 						for _, object := range output.Contents {
-							cmd.Println(*object.Key)
+							if err := writer.WriteObject(cmd.OutOrStdout(), listObjectsOpts.Bucket, object); err != nil {
+								slog.Error("failed to write object", slog.Any("error", err))
+								return
+							}
 						}
 					}
 				}
@@ -86,4 +182,7 @@ func init() {
 	listObjectsCmd.Flags().StringVarP(&listObjectsOpts.Bucket, "bucket", "b", listObjectsOpts.Bucket, "the bucket to list objects from")
 	listObjectsCmd.Flags().StringSliceVarP(&listObjectsOpts.Prefixes, "prefix", "p", listObjectsOpts.Prefixes, "the prefix to filter objects by")
 	listObjectsCmd.Flags().StringVarP(&listObjectsOpts.Delimiter, "delimiter", "d", listObjectsOpts.Delimiter, "the delimiter to use when listing objects")
+	listObjectsCmd.Flags().VarP(enumflag.New(&listObjectsOpts.Format, "format", ListObjectsFormatStringMap, enumflag.EnumCaseInsensitive), "format", "f", "the output format")
+	listObjectsCmd.Flags().StringVar(&listObjectsOpts.DetailType, "detail-type", listObjectsOpts.DetailType, "the detail type for cwl-s3-event format")
+	listObjectsCmd.Flags().StringVar(&listObjectsOpts.Reason, "reason", listObjectsOpts.Reason, "the reason for cwl-s3-event format")
 }
--- go.mod
+++ go.mod
@@ -3,10 +3,12 @@ module saw
 go 1.23
 
 require (
+	github.com/aws/aws-lambda-go v1.47.0
 	github.com/aws/aws-sdk-go-v2/config v1.28.6
 	github.com/aws/aws-sdk-go-v2/service/s3 v1.71.0
 	github.com/aws/aws-sdk-go-v2/service/sqs v1.37.2
 	github.com/spf13/cobra v1.8.1
+	github.com/thediveo/enumflag/v2 v2.0.5
 )
 
 require (
@@ -26,6 +28,8 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.28.6 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sts v1.33.2 // indirect
 	github.com/aws/smithy-go v1.22.1 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/exp v0.0.0-20231006140011-7918f672742d // indirect
 )
